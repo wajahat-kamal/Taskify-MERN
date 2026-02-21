ad17678233f1641633064b6d5b8db05419071ba0
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-21 12:52:42 +0500

    Update Navbar component to include additional navigation items and enhance styling. Adjust button color for better visibility and modify spacing for improved layout consistency.

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,21 +1,22 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Navbar() {
-    const nav_items = ["Home", "About", "Tasks"]
+    const nav_items = ["Home", "About", "Tasks", "Pricing", "Products"]
     return (
-        <nav className='bg-[#000310] w-full h-20 flex items-center justify-between px-20 py-4'>
+        <nav className='bg-[#000310] w-full h-20 flex items-center justify-between px-30 py-4'>
             <div>
-                {/* <img src="" alt="logo" /> */}
+                <Image src="" alt="logo" />
                 <h1>Taskify</h1>
             </div>
-            <ul className='w-full flex justify-center items-center gap-6'>
+            <ul className='w-full flex justify-center items-center gap-8'>
                 {nav_items.map((item, index) => (
                     <li key={item + index} >
                         {item}
                     </li>
                 ))}
             </ul>
-            <button className='bg-[#8A77F2] px-6 py-2.5 rounded-4xl text-md'>Signup</button>
+            <button className='bg-[#5A5DC1] px-6 py-2.5 rounded-4xl text-md'>Signup</button>
         </nav>
     )
 }
