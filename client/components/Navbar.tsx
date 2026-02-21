0419de8a67f6573c04a38de9b5bc384a251b3369
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-21 12:16:36 +0500

    Refactor Navbar component to enhance layout and styling, including responsive design adjustments and improved item rendering.

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -2,24 +2,22 @@ import React from 'react'
 
 function Navbar() {
     const nav_items = ["Home", "About", "Tasks"]
-  return (
-    <nav>
-        <div>
-            <img src="" alt="" />
-            <h1>Taskify</h1>
-        </div>
-        <div>
-            {nav_items.map((item, index) => (
-                <ul key={item+index}>
-                    <li>{item}</li>
-                </ul>
-            ))}
-        </div>
-        <div>
-            <button>Sign up</button>
-        </div>
-    </nav>
-  )
+    return (
+        <nav className='w-full h-20 flex items-center justify-between px-20 py-4'>
+            <div>
+                <img src="" alt="logo" />
+                <h1>Taskify</h1>
+            </div>
+            <ul className='w-full flex justify-center items-center gap-3'>
+                {nav_items.map((item, index) => (
+                    <li key={item + index} >
+                        {item}
+                    </li>
+                ))}
+            </ul>
+                <button>Sign up</button>
+        </nav>
+    )
 }
 
 export default Navbar
