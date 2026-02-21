8ea6dfc4d649ffba79132fc4198854ed688bf441
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-21 07:29:54 +0500

    Add CORS middleware to app.ts to enable cross-origin requests from specified origins

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from "cors"
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.route.js';
 import taskRouter from './routes/task.route.js';
@@ -8,10 +9,15 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: ["http://localhost:3000", "https://taskify-wk.vercel.app"],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}))
 
 app.use('/api/auth', authRouter);
 app.use('/api/tasks', protect, taskRouter);
-
 
 app.get("/", (req, res) => {
     res.json("Hello World!")
