d911e6dab739ea9314fb98ea388d3bb93c3a266d
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-21 12:57:08 +0500

    Refactor Navbar component to enhance layout and styling by centering logo and title, adjusting dimensions, and improving text presentation for better visual appeal.

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -5,9 +5,9 @@ function Navbar() {
     const nav_items = ["Home", "About", "Tasks", "Pricing", "Products"]
     return (
         <nav className='bg-[#000310] w-full h-20 flex items-center justify-between px-30 py-4'>
-            <div>
-                <Image src="/logo.png" alt="logo" />
-                <h1>Taskify</h1>
+            <div className='flex justify-center items-center gap-2'>
+                <Image src="/logo.png" alt="logo" width={40} height={40} className='rounded'/>
+                <h1 className='text-2xl text-[#5A5DC1]'>TASKIFY</h1>
             </div>
             <ul className='w-full flex justify-center items-center gap-8'>
                 {nav_items.map((item, index) => (
