24a4d17831c572a2db0699643631d5ab0adcf8f7
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-23 11:44:26 +0500

    Add ToastContainer to RootLayout: Enhance user notification experience by integrating ToastContainer for consistent toast notifications across the application.

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,12 +4,10 @@ import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Testimonials from "@/components/Testimonials";
-import { ToastContainer } from "react-toastify";
 
 export default function Home() {
   return (
     <div className="bg-[#000310]">
-      <ToastContainer/>
       <Navbar />
       <Hero />
       <AboutSection/>
