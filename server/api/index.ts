8a8b3a3e70ef15cfbae46b5cfd62a4ceadd29726
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-24 12:26:35 +0500

    Refactor API handler: Update the default export in index.ts to an async function that connects to the database before handling requests, improving connection management.

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,9 +1,10 @@
-import dotenv from 'dotenv';
-import connectDB from '../src/config/db.js';
-import app from '../src/app.js';
+import dotenv from "dotenv";
+import connectDB from "../src/config/db.js";
+import app from "../src/app.js";
 
 dotenv.config();
 
-connectDB();
-
-export default app;
+export default async function handler(req: any, res: any) {
+    await connectDB();
+    return app(req, res);
+}
