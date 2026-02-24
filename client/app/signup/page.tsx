f65353d56328d0845167aa5779788b37ca037b03
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-24 19:22:34 +0500

    Remove onClick handler from submit button in SignupPage to streamline the registration process.

--- client/app/signup/page.tsx
+++ client/app/signup/page.tsx
@@ -161,7 +161,6 @@ export default function SignupPage() {
 
                         {/* Submit */}
                         <motion.div
-                            onClick={submitRegisterForm}
                             variants={fadeUp}
                             initial="hidden"
                             animate="visible"
