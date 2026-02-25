c4dcda33ea2e9fe9d9948d3e7ff61611132db15b
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-25 16:21:21 +0500

    Enhance authentication flow by integrating Redux state management into Auth component, improving user experience and code organization.

---
+++ client/store/slices/authSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface AuthState {
+    user: null | { name: string, email: string };
+    token: null | string;
+}
+
+const initialState: AuthState = {
+    user: null,
+    token: null
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        setUser: (state, action: PayloadAction<AuthState>) => {
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        },
+        logout: (state) => {
+            state.user = null;
+            state.token = null
+        }
+    }
+})
+
