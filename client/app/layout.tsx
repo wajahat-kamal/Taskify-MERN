a492d97af8ee540c686ac4c81e2eec639599847d
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-25 16:13:40 +0500

    Add ReduxProvider to RootLayout for improved state management integration

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import "./globals.css";
+import ReduxProvider from "@/store/Provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ToastContainer />
-        {children}
+        <ReduxProvider>
+          <ToastContainer />
+          {children}
+        </ReduxProvider>
       </body>
     </html>
   );
