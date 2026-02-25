07d02b39daf8aabf918b23d4f47633d924d0f616
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-25 17:05:04 +0500

    Add /me route to authRouter for user information retrieval, integrating getMe function from auth.controller to enhance session management.

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,10 +1,11 @@
 import express from "express"
-import { createAccount, login, logout } from "../controllers/auth.controller.js"
+import { createAccount, getMe, login, logout } from "../controllers/auth.controller.js"
 
 const authRouter: express.Router = express.Router()
 
 authRouter.post("/register", createAccount)
 authRouter.post("/login", login)
 authRouter.get("/logout", logout)
+authRouter.get("/me", getMe)
 
 export default authRouter;
