9f120dc0fa38d7864978a98ab75f23bae41c42b4
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-25 16:31:40 +0500

    Add authReducer to Redux store configuration for managing authentication state

--- client/store/store.ts
+++ client/store/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import authReducer from "./slices/authSlice";
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        auth: authReducer
+    }
 })
 
 export type RootState = ReturnType <typeof store.getState>
