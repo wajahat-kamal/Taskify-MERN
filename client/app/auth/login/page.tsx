ba1e080ff7882a935e96b87aea30b4bf2d033312
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-25 16:02:20 +0500

    Add console log for token in LoginPage after successful login

--- client/app/auth/login/page.tsx
+++ client/app/auth/login/page.tsx
@@ -39,6 +39,8 @@ export default function LoginPage() {
                 toast.success(data.message);
                 setForm({ email: "", password: "" });
                 router.push("/"); // redirect after success
+                console.log(data.token);
+                
             } else {
                 toast.error(data.message);
             }
