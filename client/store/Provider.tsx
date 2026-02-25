8f3e514d72f08e6d97adb6dfb59dcd47ab52b4f7
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-25 17:10:31 +0500

    Integrate user session retrieval in ReduxProvider using axios to fetch user data on component mount, enhancing user state management in the application.

--- client/store/Provider.tsx
+++ client/store/Provider.tsx
@@ -1,8 +1,20 @@
 // store/Provider.tsx
 "use client";
+import { useEffect } from "react";
 import { store } from "./store";
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
+import axios from "axios";
+import { setUser } from "./slices/authSlice";
 
 export default function ReduxProvider({ children }: { children: React.ReactNode }) {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        axios.get(
+            // `${process.env.NEXT_PUBLIC_API_URL}/auth/me`,
+            "http://localhost:8000/auth/me", 
+            {withCredentials: true}
+        ).then((res) => dispatch(setUser({ user: res.data.user }))).catch(() => { });
+    })
+
     return <Provider store={store}>{children}</Provider>;
 }
