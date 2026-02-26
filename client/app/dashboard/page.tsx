eb06dde2ac0c31f84f136470a329590b1149243f
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-26 16:22:48 +0500

    Update user display in Dashboard sidebar to show initials from full name, enhancing user identification.

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -5,12 +5,13 @@ import { useSelector } from 'react-redux'
 
 function page() {
     const { user } = useSelector((state: RootState) => state.auth)
+
     return (
         <div className='bg-[#0e0f14] min-h-screen text-white'>
             <aside>
                 <div className="w-8 h-8 rounded-lg bg-indigo-500 flex items-center justify-center text-sm font-bold">T</div>
                 <div>
-                    {user?.name[0].toUpperCase()}
+                    {user?.name.split(" ").map(word => word[0]).join("").toUpperCase()}
                 </div>
             </aside>
         </div>
