99f50c5dcf2af3c2a4dc531faaea252ebadcb66f
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-26 19:48:58 +0500

    Add logo image to sidebar in Dashboard page for enhanced branding and visual appeal

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { RootState } from '@/store/store'
 import { useSelector } from 'react-redux'
+import Image from 'next/image'
 
 function Dashboard() {
     const { user } = useSelector((state: RootState) => state.auth)
@@ -9,7 +10,9 @@ function Dashboard() {
     return (
         <div className='bg-[#0e0f14] min-h-screen text-white'>
             <aside className="fixed left-0 top-0 h-full w-16 bg-[#13141b] flex flex-col justify-between items-center py-6 gap-6 border-r border-white/5 z-10">
-                <div className="w-10 h-10 rounded-lg bg-(--color-primary) flex items-center justify-center text-sm font-bold">T</div>
+                <div className="w-10 h-10 rounded-lg bg-(--color-primary) flex items-center justify-center text-sm font-bold">
+                    <Image src="/logo.png" alt="logo" width={60} height={60} />
+                </div>
                 <div className='w-10 h-10 rounded-full bg-(--color-primary) flex items-center justify-center text-xs font-bold'>
                     {user?.name.split(" ").map(word => word[0]).join("").toUpperCase()}
                 </div>
