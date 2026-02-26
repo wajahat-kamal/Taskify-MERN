5d73af44c0f1c93712b4d4802bd714216c611fb8
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-26 16:19:04 +0500

    Refactor Dashboard page component for improved readability by standardizing formatting and ensuring consistent spacing in JSX structure.

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,19 +1,20 @@
-import { RootState } from '@/store/store'
+"use client"
 import React from 'react'
+import { RootState } from '@/store/store'
 import { useSelector } from 'react-redux'
 
 function page() {
-    const {user} = useSelector((state: RootState) => state.auth)
-  return (
-    <div className='bg-[#0e0f14] min-h-screen text-white'>
-        <aside>
-        <div className="w-8 h-8 rounded-lg bg-indigo-500 flex items-center justify-center text-sm font-bold">T</div>
-            <div>
-                {user?.name[0].toUpperCase() || "W"}
-            </div>
-        </aside>
-    </div>
-  )
+    const { user } = useSelector((state: RootState) => state.auth)
+    return (
+        <div className='bg-[#0e0f14] min-h-screen text-white'>
+            <aside>
+                <div className="w-8 h-8 rounded-lg bg-indigo-500 flex items-center justify-center text-sm font-bold">T</div>
+                <div>
+                    {user?.name[0].toUpperCase()}
+                </div>
+            </aside>
+        </div>
+    )
 }
 
 export default page
