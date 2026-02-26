728e63ba033ea4f5845f9d4c12c7b8802a9f5c5c
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-26 16:25:26 +0500

    Update sidebar user initials display in Dashboard page for improved visibility and consistency, increasing icon size and adjusting font styling.

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -9,8 +9,8 @@ function page() {
     return (
         <div className='bg-[#0e0f14] min-h-screen text-white'>
             <aside className="fixed left-0 top-0 h-full w-16 bg-[#13141b] flex flex-col justify-between items-center py-6 gap-6 border-r border-white/5 z-10">
-                <div className="w-8 h-8 rounded-lg bg-(--color-primary) flex items-center justify-center text-sm font-bold">T</div>
-                <div className='w-8 h-8 rounded-full bg-(--color-primary) flex items-center justify-center text-sm font-bold'>
+                <div className="w-10 h-10 rounded-lg bg-(--color-primary) flex items-center justify-center text-sm font-bold">T</div>
+                <div className='w-10 h-10 rounded-full bg-(--color-primary) flex items-center justify-center text-xs font-bold'>
                     {user?.name.split(" ").map(word => word[0]).join("").toUpperCase()}
                 </div>
             </aside>
