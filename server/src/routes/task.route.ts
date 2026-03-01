fc38f71638d1897c173017e40d1352fbe9468094
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-01 11:47:35 +0500

    Add task statistics endpoint to task router for improved task management

--- server/src/routes/task.route.ts
+++ server/src/routes/task.route.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import { createTask, deleteTask, getTasks, updateTask } from "../controllers/task.controller.js";
+import { createTask, deleteTask, getTaskStats, getTasks, updateTask } from "../controllers/task.controller.js";
 
 const taskRouter = express.Router()
 
@@ -7,5 +7,6 @@ taskRouter.post("/create-task", createTask)
 taskRouter.get("/all-tasks", getTasks)
 taskRouter.delete("/delete-task/:id", deleteTask)
 taskRouter.put("/update-task/:id", updateTask)
+taskRouter.get("/task-stats", getTaskStats)
 
 export default taskRouter;
